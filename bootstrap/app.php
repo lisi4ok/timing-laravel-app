2bf89d0137e3a944ab43ccc77c31c6a25f26c838
Zaio Klepoyshkov <lisi4ok@gmail.com>
2025-03-21 15:12:43 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,18 +26,18 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-    $exceptions->render(function (ModelNotFoundException $e, Request $request) {
-        if ($request->is('api/*')) {
-            return response()->json([
-                'message' => 'Record not found.'
-            ], 404);
-        }
-    });
-    $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-        if ($request->is('api/*')) {
-            return response()->json([
-                'message' => 'Data not found.'
-            ], 404);
-        }
-    });
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404);
+            }
+        });
+        $exceptions->render(function (ModelNotFoundException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404);
+            }
+        });
     })->create();
